dad93c29b4ab9cb086e7240dec48dccc3058c434
HamzaMoinet <moinethamza0@gmail.com>
2024-10-22 13:46:48 +0000

    ee

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,26 @@
- #include <stdio.h>
- #include <stdlib.h>
- #include <time.h>
- 
- /**
-  * main - Entry point
-  *
-  * Return: Always 0 (Success)
- */
- 
- int main(void)
- {
-         int i = 0;
- 
-         while(i < 2)
-         {
-                 printf("abcdefghijklmnopqrstuvwxyz\n");
-                  i++;
- }
- return(0);
- }
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	for (alpha = 'a'; alpha <= 'z' ; alpha++)
+	{
+	putchar(alpha);
+	}
+	putchar('\n');
+return (0);
+}
