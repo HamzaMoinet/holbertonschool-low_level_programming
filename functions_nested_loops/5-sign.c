ac009b6a403b1995abf6603c3ef7756f3210521b
HamzaMoinet <moinethamza0@gmail.com>
2024-10-24 11:40:48 +0000

    Write a function that prints the sign of a number.

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 
 /**
- *_islower - Checks if a character is lowercase.
- *@n : variable 'n' print a number
- *Return: 1 if greater than zero
- *        0 if egal than zero
- *        -1 less than zero
-*/
+ * print_sign - Prints the sign of a number.
+ * @n: The number of which the sign will be printed.
+ *
+ * Return: 1 if the number is greater than zero,
+ *         0 if the number is zero,
+ *         -1 if the number is less than zero.
+ */
 
 int print_sign(int n)
 {
