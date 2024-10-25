53e9a39e93f068dac498d22b4ffa7011b95fbd68
HamzaMoinet <moinethamza0@gmail.com>
2024-10-25 13:07:06 +0000

    multy of 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,25 +7,42 @@
 
 void times_table(void)
 {
-	int nums, mult, calc_mult;
+	int num, mult, prod;
 
-	for (nums = '0'; nums <= '9'; nums++)
+	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
 
-		for (mult = '0'; mult <= '9'; mult++)
+		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
 
-			calc_mult = nums * mult;
+			prod = num * mult;
 
-			if (calc_mult <= 9)
+			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((calc_mult / 10) + '0');
-			_putchar((calc_mult % 10) + '0');
+				_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
+		for (num = 0; num <= 9; num++)
+		{
+			_putchar('0');
+			for (mult = 1; mult <= 9; mult++)
+			{
+				_putchar(',');
+				_putchar('.');
+				prod = num * mult;
+
+				if (prod <= 9)
+					_putchar(' ');
+				else
+					_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
+			}
+			_putchar('\n');
+		}
 }
