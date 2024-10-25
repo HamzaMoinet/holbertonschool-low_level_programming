ba267f81d0860625e72a4c8d964f882fa3a5106c
HamzaMoinet <moinethamza0@gmail.com>
2024-10-25 09:31:22 +0000

    hours, minute

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * jack_bauer - print every minute and hours
+ *              start at 00:00 to 23:59
+ */
+
+void jack_bauer(void)
+{
+	int hours, minute;
+
+	for (hours = 0; hours <= 24; hours++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
