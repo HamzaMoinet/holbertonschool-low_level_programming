b5fec0f3874635a65006b930e1e449746807ecdd
HamzaMoinet <moinethamza0@gmail.com>
2024-10-28 11:14:46 +0000

    print numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *print_numbers - print a number
+*/
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		_putchar((num % 10) + '0');
+
+	_putchar('\n');
+}
