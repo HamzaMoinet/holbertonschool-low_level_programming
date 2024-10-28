4fb3d82e027a77dc40493503cb3efa86571b644f
HamzaMoinet <moinethamza0@gmail.com>
2024-10-28 10:02:49 +0000

    Write a function that checks for uppercase character.

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 
 /**
- *_ispper - check
+ *_isupper - check
  *@c : variable 'c' in uppercase
  *Return: 1 or 0 (1 is uppercase 0 is otherwise)
 */
 
+#include "main.h"
+#include <stdio.h>
 
 int _isupper(int c)
 {
