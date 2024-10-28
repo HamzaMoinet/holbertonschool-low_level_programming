87becb2176b35429550026f5c78017e9cab8c622
HamzaMoinet <moinethamza0@gmail.com>
2024-10-28 14:02:58 +0000

    mm

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - more numbers
+ *
+*/
+
+void more_numbers(void)
+{
+	int i = 0, num;
+	while(i < 10)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num <= 9)
+				_putchar((num / 10) + '0');
+
+			_putchar((num % 10) + '0');
+		}
+		i++;
+		_putchar('\n');
+	}
+}
