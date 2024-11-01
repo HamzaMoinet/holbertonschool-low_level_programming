b30c53fb3266e8f1e25fa7f059f77215da9484be
HamzaMoinet <moinethamza0@gmail.com>
2024-11-01 14:56:03 +0000

    strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
+ * _strcmp - function that compares two strings
+ * @s2: paramater for string 2
+ * @s1: paramater for string 1
+ * Return: Always 0.
 */
 
 int _strcmp(char *s1, char *s2)
