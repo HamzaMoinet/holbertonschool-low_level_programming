adbf30487fd2b89855ea16f5a238a6d51535a787
HamzaMoinet <moinethamza0@gmail.com>
2024-11-06 16:16:17 +0100

    Write a function that returns the natural square root of a number.

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *_sqrt - calcule
+ *@a: nombre a
+ *@b: resultat
+ *Return: -1 if b > of result a * a
+		a if b = a
+ */
+int _sqrt(int a, int b)
+{
+	if (a * a > b)
+	{
+		return (-1);
+	}
+	else if (a * a == b)
+	{
+		return (a);
+	}
+	return ( _sqrt(a + 1, b));
+}
+/**
+ * _sqrt_recursion - result of calcul
+ * @n: nomber
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(1, n));
+}
