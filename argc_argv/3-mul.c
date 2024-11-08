f476ccade8c1a5e4fd247d9462ac02e8443228e9
HamzaMoinet <moinethamza0@gmail.com>
2024-11-08 09:19:40 +0100

    mult

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -15,8 +14,8 @@ int main(int argc, char *argv[])
 
 	if 	(argc != 3)
 	{
-			printf("Error\n");
-			return (1);
+		printf("Error\n");
+		return (1);
 	}
 
 	num1 = atoi(argv[1]);
