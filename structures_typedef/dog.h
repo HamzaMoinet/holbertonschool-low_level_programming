23ffd24db26e0c0b477c67336540f0c59bfdd6a3
HamzaMoinet <moinethamza0@gmail.com>
2024-11-13 19:50:12 +0100

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,3 +1,4 @@
+#ifndef _struct
 #define _struct
 /**
  * struct dog - structure of dog
@@ -11,9 +12,10 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 char *_strcpy(char *dest, char *src);
 void free_dog(dog_t *d);
+#endif
