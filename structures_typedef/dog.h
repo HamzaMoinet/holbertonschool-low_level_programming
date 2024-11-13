636c740101902fe1d9c54448f3e9cc1c0ca97ee9
HamzaMoinet <moinethamza0@gmail.com>
2024-11-13 19:44:45 +0100

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#define _struct
+/**
+ * struct dog - structure of dog
+ * @name: nom
+ * @age: age of dog
+ * @owner: proprio
+ * Description: Fiche d'un chien que je ne connais pas
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}dog_t;
