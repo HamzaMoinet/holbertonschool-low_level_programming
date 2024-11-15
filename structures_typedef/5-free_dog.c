5c311e63576e9e90ed81a58faa4e1ba16a68c83a
HamzaMoinet <moinethamza0@gmail.com>
2024-11-15 21:22:07 +0100

    free dog

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,19 +1,17 @@
-#include <stdlib.h>
 #include "dog.h"
+#include <stdlib.h>
+
 /**
- * free_dog - frees a dog
- *
- * @d: dog to free
- *
- * Return: void
+ * free_dog - function to set the dog free.
+ * @d: poppy need a new owner because he's mean
  */
+
 void free_dog(dog_t *d)
 {
-	if (d == 0)
-		return;
-	if (d->name)
+	if (d != NULL)
+	{
 		free(d->name);
-	if (d->owner)
 		free(d->owner);
-	free(d);
+		free(d);
+	}
 }
