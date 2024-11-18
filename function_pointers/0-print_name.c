013e9343a380d62baed554cc8096fc2c8d90be40
HamzaMoinet <moinethamza0@gmail.com>
2024-11-18 14:51:42 +0100

    print name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,10 +1,11 @@
+#include "function_pointers.h"
 #include <stdlib.h>
 
 /**
- *print_name - print a nam
- *@name : Bob and BOB DYLAN
- *@f: print nam
- *Return: return
+ * print_name - function that print the name
+ *
+ * @name: is a function
+ * @f: pointer of function name
  */
 void print_name(char *name, void (*f)(char *))
 {
@@ -13,5 +14,4 @@ void print_name(char *name, void (*f)(char *))
 		return;
 	}
 	f(name);
-
 }
