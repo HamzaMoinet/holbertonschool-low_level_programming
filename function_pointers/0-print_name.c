8e35de523ac89a9dace087af8de39eaa111005db
HamzaMoinet <moinethamza0@gmail.com>
2024-11-18 12:57:21 +0100

    task 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *print_name - print a nam
+ *@name : Bob and BOB DYLAN
+ *@f:
+ *Return:
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == 0)
+	{
+		f(name);
+	}
+	return;
+}
