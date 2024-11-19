265477b98b15ea77158e87f4469d9d5191857d02
HamzaMoinet <moinethamza0@gmail.com>
2024-11-19 15:44:25 +0100

    task 1

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * @brief
+ *
+ *
+ *
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+
+	va_start (ap, n);
+
+	if (separator != NULL)
+	{
+		separator = ", ";
+	}
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+		printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
+}
