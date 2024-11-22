d7a23759e2c3385e100f369d04a22e007539bd6a
HamzaMoinet <moinethamza0@gmail.com>
2024-11-22 10:41:35 +0100

    variadic_functions.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,4 +1,22 @@
+#ifndef VARIADIC_FUNCTIONS
+#define VARIADIC_FUNCTIONS
+
+#include <stdarg.h>
+/**
+ * struct print - print
+ * @type: The operator
+ * @f: The function associated
+ */
+
+typedef struct print
+{
+	char *type; /* Type to print (e.g. "c", "i", "f", "s") */
+	void (*f)(va_list ap); /* Pointer function to the right function*/
+} print_type;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+#endif
