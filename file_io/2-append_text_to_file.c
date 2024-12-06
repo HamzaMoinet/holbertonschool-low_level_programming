ae8ba2068fedbd170aecbdd7c0f5d533afdf9e93
HamzaMoinet <moinethamza0@gmail.com>
2024-12-06 15:44:49 +0100

    task 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *append_text_to_file -
- *
- *
- *
+ *append_text_to_file - append
+ *@filename: name of file
+ *@text_content: content text
+ *Return: 0
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
