c002ba3de0aad8c8044b57d8c574785dafb3a4f9
HamzaMoinet <moinethamza0@gmail.com>
2024-12-06 15:15:16 +0100

    task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * @brief
- *
- *
+ *create_file - create a file
+ *@filename: name of file
+ *@text_content: text content on file
+ *Return: 0
  */
 int create_file(const char *filename, char *text_content)
 {
