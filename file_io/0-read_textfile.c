41beb5db89b3c4a603bb6654cf110362367a3ff3
HamzaMoinet <moinethamza0@gmail.com>
2024-12-06 14:36:11 +0100

    task 0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * @brief
+ *
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t lec, print;
+	int fd = 0;
+	char *buffer;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	lec = read(fd, buffer, letters);
+	if (lec == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	print = write(STDOUT_FILENO, buffer, lec);
+	free(buffer);
+	close(fd);
+	return (print);
+}
