1b2dfcf3b15ee5d266404ed1638cfaed4ea8ded4
HamzaMoinet <moinethamza0@gmail.com>
2024-12-07 15:58:02 +0100

    Write a function that adds a new node at the end of a dlistint_t list.

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,13 +1,12 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
+
 /**
 * add_dnodeint_end - adds a new node at the end of a dlistint_t list
 * @head: double pointer to the head of the list
 * @n: integer add to the new mode
 * Return: adress of the new element or NULL if it failed
 */
+
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node, *temp;
